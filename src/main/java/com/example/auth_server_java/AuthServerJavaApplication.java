614618fe82b9afc24d1cc07e6594ea84a7191a9c
fleskesvor <eivindkn@gmail.com>
2024-08-14 13:02:05 +0200

    First commit

---
+++ src/main/java/com/example/auth_server_java/AuthServerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth_server_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/auth_server_java/AuthServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth_server_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
